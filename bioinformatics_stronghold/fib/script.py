319b8845c1406b01446f3dac719f26b8dab45109
JakeDam <robert.dam2@gmail.com>
2021-07-01 21:00:43 -0400

    worked on fibonacci

--- bioinformatics_stronghold/fib/script.py
+++ bioinformatics_stronghold/fib/script.py
@@ -1,7 +1,16 @@
 def fibonacci(n, k):
-    count = 1
+    mature = 1
+    baby = 0
     for i in range(0, n-1):
-        count += (k * i)
-    return count
+        if i == 0:
+            baby += k
+        elif i == 1:
+            mature += baby
+        else:
+            mature += baby
+            baby += (mature * k)
+    total = mature + baby
+    return mature
+    
 
-print(fibonacci(35, 5))
+print(fibonacci(5, 3))
