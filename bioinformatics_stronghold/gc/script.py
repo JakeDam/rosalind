b1b546c86e196989f810285f216138a0273c0a50
JakeDam <robert.dam2@gmail.com>
2021-07-09 13:51:14 -0400

    completed gc

--- bioinformatics_stronghold/gc/script.py
+++ bioinformatics_stronghold/gc/script.py
@@ -7,15 +7,31 @@ longest_GC = 0
 
 current_seq = None
 
+fasta_seq = ''
+
 for line in fasta:
     if line[0] == '>':
         current_seq = line
-    else:
         gc_count = 0
-        for base in line:
+        for base in fasta_seq:
             if base == 'G' or base == 'C':
                 gc_count += 1
-        gc_percent = gc_count/len(line) * 100
+        if len(fasta_seq) != 0:
+            gc_percent = gc_count/len(fasta_seq) * 100
+            if gc_percent > longest_GC:
+                longest_GC = gc_percent
+                longest_seq = current_seq
+        fasta_seq = ''
+    else:
+        fasta_seq += line.rstrip()
+
+gc_count = 0
+
+for base in fasta_seq:
+    if base == 'G' or base == 'C':
+        gc_count += 1
+    if len(fasta_seq) != 0:
+        gc_percent = gc_count/len(fasta_seq) * 100
         if gc_percent > longest_GC:
             longest_GC = gc_percent
             longest_seq = current_seq
