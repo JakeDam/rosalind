bcbd9b6fe9b5b4adf8750ee91b0a473a7961b44f
JakeDam <robert.dam2@gmail.com>
2021-07-18 18:11:54 -0400

    wrote README and first column of codon table

---
+++ bioinformatics_stronghold/prot/README.md
@@ -0,0 +1,2 @@
+# Translating RNA into Protein
+Takes in a mRNA sequence and retruns the protein string that it codes for
---
+++ bioinformatics_stronghold/prot/script.py
@@ -0,0 +1,6 @@
+codons = {
+    'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S', 
+    'UAU': 'Y', 'UAC': 'Y', 'UAA': 'STOP', 'UAG': 'STOP',
+    'UGU': 'C', 'UGC': 'C', 'UGA': 'STOP', 'UGG': 'W'
+}
