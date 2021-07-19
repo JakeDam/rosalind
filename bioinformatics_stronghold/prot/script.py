8b1bd40136f4a393952c9b6a51c4b89e919baf02
JakeDam <robert.dam2@gmail.com>
2021-07-19 16:57:53 -0400

    completed translation script

--- bioinformatics_stronghold/prot/script.py
+++ bioinformatics_stronghold/prot/script.py
@@ -2,5 +2,34 @@ codons = {
     'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
     'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S', 
     'UAU': 'Y', 'UAC': 'Y', 'UAA': 'STOP', 'UAG': 'STOP',
-    'UGU': 'C', 'UGC': 'C', 'UGA': 'STOP', 'UGG': 'W'
+    'UGU': 'C', 'UGC': 'C', 'UGA': 'STOP', 'UGG': 'W',
+    'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L', 
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P', 
+    'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q', 
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M', 
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K', 
+    'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R', 
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V', 
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E', 
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
 }
+
+
+def translate(mrna):
+    protein_string = ""
+
+    for i in range(0, len(mrna) - 3, 3):
+        codon = mrna[i:i+3]
+        if codon in codons and codons[codon] != 'STOP':
+            protein_string += codons[codon]
+        elif codon in codons and codons[codon] == 'STOP':
+            return protein_string
+        else:
+            continue
+    return protein_string
+    
+transcript = ''
+print(translate(transcript))
